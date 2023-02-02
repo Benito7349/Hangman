2682a0ee4eda0768c81cc065debc48ddc690604f
Andrés Martínez <tomasa2009@T3179>
2023-02-02 15:27:34 -0500

    Definition of functions to choose word and input letters

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from random import randint
 import utils
 
 def run():
-    list_words_spanish = list(utils.read())
-    number = randint(0,2047)
-    word_chosen = list_words_spanish[number]
-    #print(list_words_spanish)
-    print(word_chosen)
-    print(len(word_chosen))
+  print(utils.welcome())
+  words_spanish = utils.read()
+  word_chosen = utils.choose_word(words_spanish)
+  spaces = utils.show_number_letters(word_chosen)
+
+  for i in spaces:
+    print(i, end=" ")
+  print()
+  print()
+
+  letter_gamer = utils.letter_gamer()
+
+  if letter_gamer in word_chosen:
+    print("OK")
+  else:
+    print("Bad")
+
+  print(word_chosen)
+  print(letter_gamer)
  
 if __name__ == '__main__': #Es para ejecutar el archivo main.py desde la consola.
   run()
--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
 import unidecode 
+from random import randint
 
-def read():
+def welcome(): # Función que imprime mensaje de bienvenida.
+    print("#" * 34)
+    print("¡BIENVENIDO AL JUEGO DEL AHORCADO!")
+    print("#" * 34)
+    good_luck = "¡Buena suerte!"
+    return good_luck
+
+def read(): # Función para leer .txt de palabras en Español y devolverlas en una lista.
     words_spanish = []
     with open("spanish.txt", "r") as file:
         for line in file:
@@ -8,3 +16,18 @@ def read():
             word = unidecode.unidecode(word) # Elimina acento de las palabras.
             words_spanish.append(word)
     return words_spanish
+
+def choose_word(words_spanish): # Función para escoger la palabra para jugar.
+    list_words_spanish = words_spanish
+    number = randint(0,2047)
+    word_chosen = list_words_spanish[number]
+    return word_chosen
+
+def show_number_letters(word_chosen): # Función que define los espacios de las letras a descubrir.
+    number_letters = len(word_chosen)
+    spaces = ["_" for i in range(number_letters)]
+    return spaces
+
+def letter_gamer(): # Función para ingresar la letra a jugar.
+    letter = input("Ingrese una letra: ")
+    return letter
